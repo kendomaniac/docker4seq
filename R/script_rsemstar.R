13ed3c05a0afe54e9b77ae1b6956271a88ef210d
kendomaniac <raffaele.calogero@gmail.com>
2017-01-02 17:32:31 +0100

    rsemstar ready fpr pe nostrand to be checked for SE strand and access

---
+++ R/script_rsemstar.R
@@ -0,0 +1,135 @@
+#' @title Running RSEM, Li and Dewey BMC Bioinformatics 2011 12:323
+#' @description This function executes the docker container rsemstar1, where RSEM is used to calculate gene/isoforms counts using as mapper STAR, Dubin et al. Bioinformatics. 2013 Jan 1;29(1):15-21
+#'
+#' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
+#' @param fastq.folder, a character string indicating where gzip fastq files are located
+#' @param scratch.folder, a character string indicating the scratch folder where docker container will be mounted
+#' @param genome.folder, a character string indicating the folder where the indexed reference genome for bwa is located
+#' @param seq.type, a character string indicating the type of reads to be trimmed. Two options: \code{"se"} or \code{"pe"} respectively for single end and pair end sequencing
+#' @param strandness, a character string indicating the type ofsequencing protocol used for the analysis. Three options: \code{"none"}, \code{"forward"}, \code{"reverse"} respectively for non strand selection, forward for Illumina strandness protocols, reverse for ACCESS Illumina protocol
+#' @param threads, a number indicating the number of cores to be used from the application
+#'
+#' @return three files: dedup_reads.bam, which is sorted and duplicates marked bam file, dedup_reads.bai, which is the index of the dedup_reads.bam, and dedup_reads.stats, which provides mapping statistics
+#' @examples
+#'\dontrun{
+#'     #downloading fastq files
+#'     wget http://130.192.119.59/public/test_R1.fastq.gz
+#'     wget http://130.192.119.59/public/test_R2.fastq.gz
+#'     #running rsemstar nostrand pe
+#'     rsemstar(group="sudo",fastq.folder=getwd(), scratch.folder="/data/scratch",
+#'     genome.folder="/data/scratch/hg38star", seq.type="pe", strandness=FALSE, threads=24)
+#'
+#' }
+
+rsemstar <- function(group=c("sudo","docker"),fastq.folder=getwd(), scratch.folder="/data/scratch", genome.folder, seq.type=c("se","pe"), strandness=c("none","forward","reverse"), threads=1){
+	tmp.folder <- gsub(":","-",gsub(" ","-",date()))
+	cat("\ncreating a folder in scratch folder\n")
+    dir.create(file.path(scratch.folder, tmp.folder))
+    dir.create(file.path(scratch.folder, tmp.folder,"/tmp"))
+	dir <- dir()
+	dir.info <- dir[which(dir=="run.info")]
+	if(length(dir.info)>0){
+	  system(paste("chmod 777 -R", file.path(scratch.folder, tmp.folder)))
+	  system(paste("cp run.info ", scratch.folder,"/",tmp.folder,"/run.info", sep=""))
+
+	}
+	dir <- dir[grep(".fastq.gz", dir)]
+	dir.trim <- dir[grep("trimmed", dir)]
+	cat("\ncopying \n")
+	if(length(dir)==0){
+		cat(paste("It seems that in ", getwd(), "there are not fastq.gz files"))
+		return(1)
+	}else if(length(dir.trim)>0){
+	  dir <- dir.trim
+	  system(paste("chmod 777 -R", file.path(scratch.folder, tmp.folder)))
+	  for(i in dir){
+	    system(paste("cp ",getwd(),"/",i, " ",scratch.folder,"/",tmp.folder,"/",i, sep=""))
+	  }
+	  system(paste("chmod 777 -R", file.path(scratch.folder, tmp.folder)))
+	}else if(length(dir)>2){
+		cat(paste("It seems that in ", getwd(), "there are more than two fastq.gz files"))
+		return(2)
+	}else{
+		system(paste("chmod 777 -R", file.path(scratch.folder, tmp.folder)))
+		for(i in dir){
+		      system(paste("cp ",getwd(),"/",i, " ",scratch.folder,"/",tmp.folder,"/",i, sep=""))
+	    }
+		system(paste("chmod 777 -R", file.path(scratch.folder, tmp.folder)))
+	}
+	fastq <- sub(".gz$", "", dir)
+	cat("\nsetting as working dir the scratch folder and running bwa docker container\n")
+
+	if(group=="sudo"){
+		system("sudo docker pull docker.io/rcaloger/rsemstar1")
+		if(seq.type=="pe"){
+		  if(strandness=="none"){
+		      system(paste("sudo docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem2_nostrand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", fastq[2]," ", genome.folder," ", fastq.folder, sep=""))
+		  }else if(strandness=="forward"){
+		    system(paste("sudo docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem2_strand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", fastq[2]," ", genome.folder," ", fastq.folder, sep=""))
+		  }else if(strandness=="reverse"){
+		    system(paste("sudo docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem2_access.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", fastq[2]," ", genome.folder," ", fastq.folder, sep=""))
+		  }
+
+		}else{
+		  if(strandness=="none"){
+	        system(paste("sudo docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem1_nostrand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", genome.folder," ", fastq.folder, sep=""))
+		  }else if(strandness=="forward"){
+		    system(paste("sudo docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem1_strand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", genome.folder," ", fastq.folder, sep=""))
+		  }else if(strandness=="reverse"){
+		    system(paste("sudo docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem1_access.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", genome.folder," ", fastq.folder, sep=""))
+		  }
+		}
+	}else{
+	  if(seq.type=="pe"){
+	    if(strandness=="none"){
+	      system(paste("docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem2_nostrand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", fastq[2]," ", genome.folder," ", fastq.folder, sep=""))
+	    }else if(strandness=="forward"){
+	      system(paste("docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem2_strand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", fastq[2]," ", genome.folder," ", fastq.folder, sep=""))
+	    }else if(strandness=="reverse"){
+	      system(paste("docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem2_access.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", fastq[2]," ", genome.folder," ", fastq.folder, sep=""))
+	    }
+
+	  }else{
+	    if(strandness=="none"){
+	      system(paste("docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem1_nostrand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", genome.folder," ", fastq.folder, sep=""))
+	    }else if(strandness=="forward"){
+	      system(paste("docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem1_strand.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", genome.folder," ", fastq.folder, sep=""))
+	    }else if(strandness=="reverse"){
+	      system(paste("docker run -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/rsemstar1 sh /bin/rsem1_access.sh ",file.path(scratch.folder, tmp.folder)," ", threads," ", fastq[1]," ", genome.folder," ", fastq.folder, sep=""))
+	    }
+	  }
+
+	}
+	star <- "xxxx"
+	while(star != "Log.final.out"){
+	  Sys.sleep(10)
+	  cat("-")
+	  #checking the stas for STAR
+	  star.tmp <- dir(paste(file.path(scratch.folder, tmp.folder),"/xxx.temp/", sep=""))
+	  star.tmp <- star.tmp[grep("Log.final.out",star.tmp)]
+	  if(length(star.tmp)>0){
+	    star <- "Log.final.out"
+	    system(paste("cp ",file.path(scratch.folder, tmp.folder),"/xxx.temp/xxxLog.final.out ", file.path(scratch.folder, tmp.folder),"/Log.final.out",sep=""))
+	  }
+	}
+	out <- "xxxx"
+	#waiting for the end of the container work
+  while(out != "out.info"){
+		Sys.sleep(10)
+		cat(".")
+		out.tmp <- dir(file.path(scratch.folder, tmp.folder))
+		out.tmp <- out.tmp[grep("out.info",out.tmp)]
+
+		if(length(out.tmp)>0){
+			out <- "out.info"
+		}
+	}
+	con <- file(paste(file.path(scratch.folder, tmp.folder),"out.info", sep="/"), "r")
+	tmp <- readLines(con)
+	close(con)
+	for(i in tmp){
+		i <- sub("mv ",paste("mv ",file.path(scratch.folder, tmp.folder),"/",sep=""),i)
+		system(i)
+	}
+}
+
