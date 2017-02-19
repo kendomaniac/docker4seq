bad993cb97c367b4c143a80e249cf5ef52a76bb7
kendomaniac <raffaele.calogero@gmail.com>
2017-02-19 09:20:09 +0100

    adding GATK

--- R/bwaIndex.R
+++ R/bwaIndex.R
@@ -1,20 +1,23 @@
-#' @title Generating bwa genome index
-#' @description This function executes the docker container bwa1 where BWA is installed. The index is created using ENSEMBL genome fasta file. User needs to provide the URL for ENSEMBL genome located in the ENSEMBL ftp
+#' @title Generating bwa genome index for GATK variant call
+#' @description This function executes the docker container bwa1 where BWA is installed. The index is created using GATK bundle data genome fasta file. User needs to dowload the file in the genome folder from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle
 #'
 #' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
 #' @param genome.folder, a character string indicating the folder where the indexed reference genome for bwa will be located
-#' @param ensembl.url, a character string indicating the URL from ENSEMBL ftp for the unmasked genome sequence of interest
+#' @param dbsnp.file, a character string indicating the name of dbSNP vcf located in the genome folder. The dbSNP vcf, dbsnp_138.b37.vcf.gz and dbsnp_138.hg19.vcf.idx.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37
+#' @param g1000.file, a character string indicating the name of 1000 genome vcf located in the genome folder. The 1000 genomes vcf, Mills_and_1000G_gold_standard.indels.b37.vcf.gz and Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.idx.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/
+#' @param fasta.file, a character string indicating the name of fasta file located in the genome folder. This fasta, ucsc.hg19.fasta.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg19/
+#' @param gatk, a boolean TRUE and FALSE that indicate if the index will be used for GATK analysis
 #'
 #' @return The indexed bwa genome reference sequence
 #' @examples
 #'\dontrun{
 #'     #running bwa index
-#'     bwaIndex(group="sudo",genome.folder="/sto2/data/scratch/hg38",
-#'     ensembl.url=
-#'     "ftp://ftp.ensembl.org/pub/release-87/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.toplevel.fa.gz")
+#'     bwaIndex(group="sudo",genome.folder="/sto2/data/scratch/hg38", fasta.file="ucsc.hg19.fasta.gz" ,
+#'     dbsnp.file="dbsnp_138.b37.vcf.gz", g1000.file="Mills_and_1000G_gold_standard.indels.b37.vcf.gz",
+#'     gatk=FALSE)
 #' }
 #' @export
-bwaIndex <- function(group=c("sudo","docker"),genome.folder=getwd(), ensembl.url){
+bwaIndex <- function(group=c("sudo","docker"),genome.folder=getwd(), fasta.file, dbsnp.file, g1000.file, gatk=FALSE){
   #running time 1
   ptm <- proc.time()
   #running time 1
@@ -25,13 +28,35 @@ bwaIndex <- function(group=c("sudo","docker"),genome.folder=getwd(), ensembl.url
   }
 
 	cat("\nsetting as working dir the genome folder and running bwa docker container\n")
+  dir <- dir(genome.folder)
+  if(dir[grep(fasta.file,dir)]!=fasta.file){
+    cat("\nfasta file is missing\n")
+    return(1)
+  }else{
+    system(paste("gzip -d ", fasta.file, sep=""))
+    system(paste("mv ", sub(".gz$","",fasta.file)," genome.fa", sep=""))
+  }
+  if(gatk){
+    if(dir[grep(dbsnp.file,dir)]!=dbsnp.file){
+      cat("\ndbSNP vcf is missing\n")
+      return(2)
+    }else{
+      system(paste("gzip -d ", dbsnp.file, sep=""))
+    }
+    if(dir[grep(g1000.file,dir)]!=g1000.file){
+      cat("\1000 genomes vcf file is missing\n")
+      return(3)
+    }else{
+      system(paste("gzip -d ", g1000.file, sep=""))
+    }
+  }
 
 	if(group=="sudo"){
 		system("sudo docker pull docker.io/rcaloger/bwa.2017.01")
-		system(paste("sudo docker run --privileged=true -v ",genome.folder,":/data/scratch"," -d docker.io/rcaloger/bwa.2017.01 sh /bin/bwa.index.sh "," ",genome.folder," ",ensembl.url, sep=""))
+		system(paste("sudo docker run --privileged=true -v ",genome.folder,":/data/scratch"," -d docker.io/rcaloger/bwa.2017.01 sh /bin/bwa.index.sh "," ",genome.folder, sep=""))
 	}else{
 		system("docker pull docker.io/rcaloger/bwa.2017.01")
-		system(paste("docker run --privileged=true -v ",genome.folder,":/data/scratch"," -d docker.io/rcaloger/bwa.2017.01 sh /bin/bwa.index.sh "," ",genome.folder," ",ensembl.url, sep=""))
+		system(paste("docker run --privileged=true -v ",genome.folder,":/data/scratch"," -d docker.io/rcaloger/bwa.2017.01 sh /bin/bwa.index.sh "," ",genome.folder, sep=""))
 	}
 	out <- "xxxx"
 	#waiting for the end of the container work
---
+++ R/gatkDna.R
@@ -0,0 +1,100 @@
+#' @title Running realignment and recalibration, GATK
+#' @description This function executes the docker container snv.1 where GATK software is used to do INDEL realignment and quality recalibration. This analysis is required only to run mutect1
+#'
+#' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
+#' @param bam.folder, a character string indicating where bam files generated with bwa.R are located
+#' @param gatk.path, a character string indicating the path to GenomeAnalysisTK-X.X-0.tar.bz2
+#' @param scratch.folder, a character string indicating the scratch folder where docker container will be mounted
+#' @param genome.folder, a character string indicating the folder where the indexed reference genome for bwa is located
+#' @param dbsnp.file, a character string indicating the name of dbSNP vcf located in the genome folder. The dbSNP vcf, dbsnp_138.b37.vcf.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37
+#' @param g1000.file, a character string indicating the name of 1000 genome vcf located in the genome folder. The 1000 genomes vcf, Mills_and_1000G_gold_standard.indels.b37.vcf.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/
+#' @param threads, a number indicating the number of cores to be used from the application
+#'
+#' @return three files: dedup_reads.bam, which is sorted and duplicates marked bam file, dedup_reads.bai, which is the index of the dedup_reads.bam, and dedup_reads.stats, which provides mapping statistics
+#' @examples
+#'\dontrun{
+#'     #downloading fastq files
+#'     system("wget http://130.192.119.59/public/test_R1.fastq.gz")
+#'     system("wget http://130.192.119.59/public/test_R2.fastq.gz")
+#'     #running bwa
+#'     gatkDna(group="sudo",bam.folder=getwd(), scratch.folder="/data/scratch",
+#'     genome.folder="/data/scratch/hg19_exome", gatk.path=getwd(), dbsnp.file="dbsnp_138.b37.vcf.gz",
+#'     g1000.file="Mills_and_1000G_gold_standard.indels.b37.vcf.gz",
+#'     threads=24, sample.id="exome")
+#' }
+#' @export
+gatkDNA <- function(group=c("sudo","docker"), bam.folder=getwd(), scratch.folder="/data/scratch", genome.folder, gatk.path, dbsnp.file, g1000.file, threads=1){
+  #running time 1
+  ptm <- proc.time()
+  #running time 1
+  test <- dockerTest()
+  if(!test){
+    cat("\nERROR: Docker seems not to be installed in your system\n")
+    return()
+  }
+  tmp.folder <- gsub(":","-",gsub(" ","-",date()))
+  cat("\ncreating a folder in scratch folder\n")
+  dir.create(file.path(scratch.folder, tmp.folder))
+  dir.create(file.path(scratch.folder, tmp.folder,"/tmp"))
+  dir <- dir()
+  dir.info <- dir[which(dir=="run.info")]
+  if(length(dir.info)>0){
+    system(paste("chmod 777 -R", file.path(scratch.folder, tmp.folder)))
+    system(paste("cp run.info ", scratch.folder,"/",tmp.folder,"/run.info", sep=""))
+
+  }
+  dir <- dir[grep("dedup_reads.bam", dir)]
+  cat("\ncopying \n")
+  if(length(dir)==0){
+    cat(paste("It seems that in ", getwd(), "there there is not dedup_reads.bam"))
+    return(1)
+  }
+  docker_bam.folder=file.path("/data/scratch", tmp.folder)
+  system(paste("chmod 777 -R", docker_bam.folder))
+  system(paste("cp ",getwd(),"/dedup_reads.bam ",docker_bam.folder,"/dedup_reads.bam", sep=""))
+  system(paste("cp ",getwd(),"/dedup_reads.bai ",docker_bam.folder,"/dedup_reads.bai", sep=""))
+  system(paste("cp ",gatk.path, " ",docker_bam.folder,"/GenomeAnalysisTK.tar.bz2", sep=""))
+  if(group=="sudo"){
+    system("sudo docker pull docker.io/rcaloger/snv.1")
+    system(paste("sudo docker run --privileged=true --cidfile ",bam.folder,"/dockerID -v ",scratch.folder,":/data/scratch -v",genome.folder,":/data/genome -d docker.io/rcaloger/snv.1 sh /bin/bwa.sh ",docker_bam.folder," ", dbsnp.file," ",g1000.file," ", threads," ", bam.folder, sep=""))
+  }else{
+    system("docker pull docker.io/rcaloger/snv.1")
+    system(paste("docker run --privileged=true -v ",scratch.folder,":/data/scratch"," -d docker.io/rcaloger/snv.1 sh /bin/bwa.sh ",docker_bam.folder," ", dbsnp.file," ",g1000.file," ", threads," ", bam.folder, sep=""))
+
+  }
+  out <- "xxxx"
+  #waiting for the end of the container work
+  while(out != "out.info"){
+    Sys.sleep(10)
+    cat(".")
+    out.tmp <- dir(docker_bam.folder)
+    out.tmp <- out.tmp[grep("out.info",out.tmp)]
+
+    if(length(out.tmp)>0){
+      out <- "out.info"
+    }
+  }
+  con <- file(paste(docker_bam.folder,"out.info", sep="/"), "r")
+  tmp <- readLines(con)
+  close(con)
+  for(i in tmp){
+    i <- sub("mv ",paste("mv ",docker_bam.folder,"/",sep=""),i)
+    system(i)
+  }
+  #running time 2
+  ptm <- proc.time() - ptm
+  con <- file(paste(bam.folder,"run.info", sep="/"), "r")
+  tmp.run <- readLines(con)
+  close(con)
+  tmp.run[length(tmp.run)+1] <- paste("user run time mins ",ptm[1]/60, sep="")
+  tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+  tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+  writeLines(tmp.run,paste(bam.folder,"run.info", sep="/"))
+  #running time 2
+  #removing temporary folder
+  cat("\n\nRemoving the rsemStar temporary file ....\n")
+#  system(paste("rm -R ",docker_bam.folder))
+  system(paste("rm  ",bam.folder,"/dockerID", sep=""))
+  #removing temporary folder
+
+}
