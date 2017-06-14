1bfcb24cbec1ba8a3e92d337046f0e940358270e
kendomaniac <raffaele.calogero@gmail.com>
2017-06-14 17:48:24 +0200

    fix bugs

--- R/sampleSize.R
+++ R/sampleSize.R
@@ -30,10 +30,10 @@ sampleSize <- function(group=c("sudo","docker"), filename, power=0.80, FDR=0.1, 
   }
   
   if(group=="sudo"){
-    params <- paste("--cidfile ",output.folder, "/dockerID -v ",output.folder,":/data/scratch -d docker.io/rcaloger/r332.2017.01 Rscript /bin/.sampleSize.R ", filename, " ", power, " ", FDR, " ", genes4dispersion, " ", log2fold.chang, sep="")
+    params <- paste("--cidfile ",output.folder, "/dockerID -v ",output.folder,":/data/scratch -d docker.io/rcaloger/r332.2017.01 Rscript /bin/.sampleSize.R ", filename, " ", power, " ", FDR, " ", genes4dispersion, " ", log2fold.change, sep="")
     runDocker(group="sudo",container="docker.io/rcaloger/r332.2017.01", params=params)
   }else{
-    params <- paste("--cidfile ",output.folder, "/dockerID -v ",output.folder,":/data/scratch -d docker.io/rcaloger/r332.2017.01 Rscript /bin/.sampleSize.R ", filename, " ", power, " ", FDR, " ", genes4dispersion, " ", log2fold.chang, sep="")
+    params <- paste("--cidfile ",output.folder, "/dockerID -v ",output.folder,":/data/scratch -d docker.io/rcaloger/r332.2017.01 Rscript /bin/.sampleSize.R ", filename, " ", power, " ", FDR, " ", genes4dispersion, " ", log2fold.change, sep="")
     runDocker(group="docker",container="docker.io/rcaloger/r332.2017.01", params=params)
   }
   
