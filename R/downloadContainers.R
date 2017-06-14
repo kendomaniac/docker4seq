120452b3b3f2441a190eb3812032140e78691233
kendomaniac <raffaele.calogero@gmail.com>
2017-06-14 17:03:03 +0200

    refining docker4seq

---
+++ R/downloadContainers.R
@@ -0,0 +1,24 @@
+#' @title Download for the first time all containers embedded in the workflows
+#' @description This is a functin that preapre the docker environment to be used for the first time the docker4seq is installed. 
+#' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
+#' @param container.file, a character string with the name of the file which indicate which are the initial set of containers to be downloaded. Initally the set is given by a file located in the folder containers of docker4seq package.
+#' @examples
+#'\dontrun{
+##'     #running runDocker
+#'      downloadContainers(group="docker", container.file)
+#'     
+#' }
+#' @export
+downloadContainers <- function(group="docker", container.file=paste(path.package(package="docker4seq"),"contaniers/contaniers.txt",sep="/")){
+   containers <- readLines(container.file)
+   for(i in containers){
+     if(group=="sudo"){
+       system(paste("sudo docker pull ",i, sep=""))
+     }else if(group=="docker"){
+       system(paste("docker pull ",i, sep=""))
+     }else{
+       cat("\nThe group provides is neither sudo or docker\n")
+       return(2)
+     }
+   }
+}
