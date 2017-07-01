4297747fd7c4592b8913d0b59f31398602f10293
kendomaniac <raffaele.calogero@gmail.com>
2017-07-01 14:50:24 +0200

    fix bug

--- R/cascImpute.R
+++ R/cascImpute.R
@@ -57,5 +57,16 @@ cascImpute <- function(group=c("sudo","docker"), data.folder, counts.matrix, dro
     writeLines(tmp.run,"run.info")
   }
 
+  #saving log and removing docker container
+  container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
+  system(paste("docker logs ", container.id, " >& ", substr(container.id,1,12),".log", sep=""))
+  system(paste("docker rm ", container.id, sep=""))
+  #removing temporary folder
+  cat("\n\nRemoving the temporary file ....\n")
+  system("rm -fR anno.info")
+  system("rm -fR dockerID")
+  system("rm  -fR tempFolderID")
+  system(paste("cp ",paste(path.package(package="docker4seq"),"containers/containers.txt",sep="/")," ",data.folder, sep=""))
+
 
 }
