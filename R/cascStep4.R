c5b9ecaef9b8bd07b78522488c4d0d10d070e045
kendomaniac <raffaele.calogero@gmail.com>
2017-07-04 12:34:33 +0200

    fix bugs

--- R/cascStep4.R
+++ R/cascStep4.R
@@ -45,11 +45,11 @@ cascStep4 <- function(group=c("sudo","docker"), scratch.folder, data.folder=getw
   scrat_tmp.folder=file.path(scratch.folder, tmp.folder)
   writeLines(scrat_tmp.folder,paste(data.folder,"/tempFolderID", sep=""))
   cat("\ncreating a folder in scratch folder\n")
-  dir.create(file.path(scratch.folder, tmp.folder))
-  dir.create(file.path(scratch.folder, tmp.folder,"/Data"))
-  system(paste("cp -R ./Data", scrat_tmp.folder,sep=""))
-  system(paste("cp -R ./Results", scrat_tmp.folder,sep=""))
-  system(paste("cp -R ./output", scrat_tmp.folder,sep=""))
+  dir.create(file.path(scrat_tmp.folder))
+ # dir.create(file.path(scrat_tmp.folder,"/Data"))
+  system(paste("cp -R ./Data  ", scrat_tmp.folder,sep=""))
+  system(paste("cp -R ./Result  ", scrat_tmp.folder,sep=""))
+  system(paste("cp -R ./output  ", scrat_tmp.folder,sep=""))
 
 #  writeLines(file.path(scratch.folder, tmp.folder),"path.txt")
 
