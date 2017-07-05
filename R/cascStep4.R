3643254017e49d067c59677ed2c0c46ba476bf6b
kendomaniac <raffaele.calogero@gmail.com>
2017-07-05 09:46:27 +0200

    refining

--- R/cascStep4.R
+++ R/cascStep4.R
@@ -1,5 +1,6 @@
-#' @title Running CASC step4, step1 of feature selection
-#' @description This function executes CASC step1 identification of the optimal number of clusters
+#' @title Running CASC step4, feature selection analysis
+#' @description This function executes CASC identification of the minimal subset of genes suitable to keep the cluster discrimination.
+#' @details The first step of the analysis requires  that  both dispersion.min  and dispersion.max are set to NULL. This analysis will produce a plot describing the set of gene features specific for each  cluster that can be use  to refine the clustering. Subsequently giving  the range of  dispersion.min  and dispersion.max extrapolated from the the  first analysis will produce a SIMLR clustering describing  the effect of  the  feature selection.
 #' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
 #' @param scratch.folder, a character string indicating the path of the scratch folder
 #' @param data.folder, a character string indicating the folder where the results of cascKoptimization: Data, Results, output
@@ -8,6 +9,7 @@
 #' @param dispersion.min, min dispersion.
 #' @param dispersion.max, max dispersion.
 #' @param specific.genes, character with two option:  n, y
+
 #' @return ....
 #' @examples
 #' \dontrun{
@@ -15,15 +17,15 @@
 #'     system("wget http://130.192.119.59/public/log10_singlecells_counts.csv.gz")
 #'     system("gzip -d log10_singlecells_counts.csv.gz")
 #'     # without parameter range
-#'     cascStep4(group="docker", scratch.folder="/Users/raffaelecalogero/Desktop/scratch", data.folder=getwd(),
+#'     cascFeature(group="docker", scratch.folder="/Users/raffaelecalogero/Desktop/scratch", data.folder=getwd(),
 #'     counts.matrix="log10_singlecells_counts.csv",  k=3, dispersion.min=NULL, dispersion.max=NULL, specific.genes="n")
 #'     # indicating a parameter range
-#'     cascStep4(group="docker", scratch.folder="/Users/raffaelecalogero/Desktop/scratch", data.folder=getwd(),
+#'     cascFeature(group="docker", scratch.folder="/Users/raffaelecalogero/Desktop/scratch", data.folder=getwd(),
 #'     counts.matrix="log10_singlecells_counts.csv",  k=3, dispersion.min=2, dispersion.max=10, specific.genes="n")
 #' }
 #'
 #' @export
-cascStep4 <- function(group=c("sudo","docker"), scratch.folder, data.folder=getwd(), counts.matrix, k, dispersion.min=NULL, dispersion.max=NULL, specific.genes=c("n", "y")){
+cascSelFeatures <- function(group=c("sudo","docker"), scratch.folder, data.folder=getwd(), counts.matrix, k, dispersion.min=NULL, dispersion.max=NULL, specific.genes=c("n", "y")){
 
   counts.matrix <- sub(".csv", "", counts.matrix)
   #running time 1
