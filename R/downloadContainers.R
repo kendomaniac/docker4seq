eec9b286e1e0d2f060ae93a42ab65c63ce09b295
kendomaniac <raffaele.calogero@gmail.com>
2017-07-19 12:05:24 +0200

    changing parameter format

--- R/downloadContainers.R
+++ R/downloadContainers.R
@@ -1,15 +1,18 @@
 #' @title Download for the first time all containers embedded in the workflows
-#' @description This is a functin that preapre the docker environment to be used for the first time the docker4seq is installed. 
+#' @description This is a functin that preapre the docker environment to be used for the first time the docker4seq is installed.
 #' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
 #' @param containers.file, a character string with the name of the file which indicate which are the initial set of containers to be downloaded. Initally the set is given by a file located in the folder containers of docker4seq package.
 #' @examples
 #'\dontrun{
 ##'     #running runDocker
 #'      downloadContainers(group="docker", containers.file)
-#'     
+#'
 #' }
 #' @export
-downloadContainers <- function(group="docker", containers.file=paste(path.package(package="docker4seq"),"containers/containers.txt",sep="/")){
+downloadContainers <- function(group="docker", containers.file=NULL){
+   if(i.null(containers.file)){
+     containers.file=paste(path.package(package="docker4seq"),"containers/containers.txt",sep="/")
+   }
    containers <- readLines(containers.file)
    for(i in containers){
      if(group=="sudo"){
