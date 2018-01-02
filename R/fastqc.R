65d8da8995fb49255802d9119ed7d170454ba88a
kendomaniac <raffaele.calogero@gmail.com>
2018-01-02 16:26:15 +0100

    adding fastqc

---
+++ R/fastqc.R
@@ -0,0 +1,82 @@
+#' @title A function to handle a docker containier executing fastqc
+#' @description This function executes a ubuntu docker that produces as output FASTQCstdin_fastqc.html and stdin_fastqc.zip files
+#' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
+#' @param data.folder, a character string indicating the folder where input data are located and where output will be written
+#' @author Raffaele Calogero, raffaele.calogero [at] unito [dot] it, University of Torino
+#'
+#' @examples
+#' \dontrun{
+#'     system("wget http://130.192.119.59/public/test_R1.fastq.gz")
+#'     #running skeleton
+#'     fastqc(group="docker", data.folder=getwd())
+#' }
+#'
+#' @export
+fastqc <- function(group=c("sudo","docker"), data.folder){
+
+  #testing if docker is running
+  test <- dockerTest()
+  if(!test){
+    cat("\nERROR: Docker seems not to be installed in your system\n")
+    return()
+  }
+  #storing the position of the home folder
+  home <- getwd()
+
+  #running time 1
+  ptm <- proc.time()
+  #setting the data.folder as working folder
+  if (!file.exists(data.folder)){
+    cat(paste("\nIt seems that the ",data.folder, " folder does not exist\n"))
+    return(2)
+  }
+  setwd(data.folder)
+  #check  if scratch folder exist
+  if (!file.exists(scratch.folder)){
+    cat(paste("\nIt seems that the ",scratch.folder, " folder does not exist\n"))
+    return(3)
+  }
+  #executing the docker job
+  if(group=="sudo"){
+    params <- paste("--cidfile ",data.folder,"/dockerID -v ",data.folder,":/data/scratch -d docker.io/repbioinfo/r340.2017.01 sh /bin/fastqc.sh", sep="")
+    resultRun <- runDocker(group="sudo",container="docker.io/repbioinfo/r340.2017.01", params=params)
+  }else{
+    params <- paste("--cidfile ",data.folder,"/dockerID -v ",data.folder,":/data/scratch -d docker.io/repbioinfo/ubuntu sh /bin/fastqc.sh", sep="")
+    resultRun <- runDocker(group="docker",container="docker.io/repbioinfo/r340.2017.01", params=params)
+  }
+  #waiting for the end of the container work
+  if(resultRun=="false"){
+    cat("\nFastQC analysis is finished\n")
+  }
+  #running time 2
+  ptm <- proc.time() - ptm
+  dir <- dir(data.folder)
+  dir <- dir[grep("run.info",dir)]
+  if(length(dir)>0){
+    con <- file("run.info", "r")
+    tmp.run <- readLines(con)
+    close(con)
+    tmp.run[length(tmp.run)+1] <- paste("FastQC user run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("FastQC system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("FastQC elapsed run time mins ",ptm[3]/60, sep="")
+    writeLines(tmp.run,"run.info")
+  }else{
+    tmp.run <- NULL
+    tmp.run[1] <- paste("FastQC user run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("FastQC system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("FastQC elapsed run time mins ",ptm[3]/60, sep="")
+
+    writeLines(tmp.run,"run.info")
+  }
+
+  #saving log and removing docker container
+  container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
+  system(paste("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/fastqc_", substr(container.id,1,12),".log", sep=""))
+  system(paste("docker rm ", container.id, sep=""))
+
+  cat("\n\nRemoving the temporary file ....\n")
+  system("rm -fR dockerID")
+
+  system(paste("cp ",paste(path.package(package="docker4seq"),"containers/containers.txt",sep="/")," ",data.folder, sep=""))
+  setwd(home)
+}
