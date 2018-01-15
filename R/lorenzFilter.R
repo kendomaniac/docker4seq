67cb4ff8eb28950cf8a484442d536a6cf9a3b22d
kendomaniac <raffaele.calogero@gmail.com>
2018-01-15 22:33:53 +0100

    adding lorenzFilter and platypus

--- R/lorenzFilter.R
+++ R/lorenzFilter.R
@@ -3,7 +3,7 @@
 #' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
 #' @param scratch.folder, a character string indicating the path of the scratch folder
 #' @param data.folder, a character string indicating the folder where input data are located and where output will be written
-#' @param matrixName, counts table name. Matrix data file must be in data.folder
+#' @param matrixName, counts table name. Matrix data file must be in data.folder. The file MUST contain RAW counts, without any modification, such as log transformation, normalizatio etc. 
 #' @param p_value, threshold to be used for the filtering
 #' @param format, counts file extension, "txt", "csv"
 #' @param separator, separator used in count file, e.g. '\\t', ','
