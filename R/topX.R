b76307ed4644bbaa77ce5be21a8167123ce01795
kendomaniac <raffaele.calogero@gmail.com>
2018-02-14 21:03:47 +0100

    refining

--- R/topX.R
+++ R/topX.R
@@ -5,33 +5,45 @@
 #' @param threshold, integer used for filtering indicate the number of top expressed genes to be selected
 #' @param logged, boolean TRUE or FALSE, if FALSE gene expression data are expressed in the plot as log10.
 #'
-#' @author Name Family name, myemail [at] somewhere [dot] org, Affiliation
+#' @author Raffaele Calogero, raffaele.calogero [at] unito [dot] it, UNITO
 #'
 #' @return a filtered tab delimited file and a histogram of the gene by gene total expression
 #'
 #' @examples
 #'\dontrun{
 #'
-#'  topx(data.folder="path/of/data/folder",file.name="matrixName",threshold=100000, logged=FALSE)
+#'  topx(data.folder=getwd(),file.name="singlecells_counts.txt",threshold=10000, logged=FALSE)
 #' }
 #'
 #' @export
-topx <- function(data.folder ,file.name, threshold, log=FALSE){
+topx <- function(data.folder ,file.name, threshold, logged=FALSE){
   home <- getwd()
   setwd(data.folder)
   tmp <- read.table(file.name, header=T, sep="\t", row.names=1)
   sum.counts <- apply(tmp, 1, sum)
+  tmp <- data.frame(tmp, sum.counts)
+  tmp <- tmp[order(tmp$sum.counts, decreasing = T),]
+
   pdf("gene_expression_distribution.pdf")
   if(!logged){
-    hist(log10(sum.counts), xlab="log10 gene counts summary", breaks=100)
+    hist(log10(tmp$sum.counts), col=rgb(1,0,0,0.5), xlab="log10 gene counts summary", breaks=100)
+    hist(log10(tmp$sum.counts[1:threshold]), col=rgb(0,0,1,0.5), add=T, breaks=100)
+    legend("topright",legend=c("All","Filtered"), pch=c(15,15), col=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5)))
+    box()
   }else{
-    hist(sum.counts, xlab="log gene counts summary", breaks=100)
+    hist(tmp$sum.counts, col=rgb(1,0,0,0.5), xlab="log gene counts summary", breaks=100)
+    hist(tmp$sum.counts[1:threshold], col=rgb(0,0,1,0.5), add=T, breaks=100)
+    legend("topright",legend=c("All","Filtered"), pch=c(15,15), col=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5)))
+    box()
   }
   dev.off()
-  tmp <- tmp[order(sum.counts, decreasing = T),]
 
-  tmp.filter <- tmp[,1:threshold]
-  write.table(tmp.filter, paste(file.name, sub(".txt",paste("_",threshold,".txt", sep=""),file.name), sep=""), sep="\t", col.names = NA)
+  tmp.filter <- tmp[1:threshold,1:(dim(tmp)[2]-1)]
+  sum.counts.f <- apply(tmp.filter, 1, sum)
+
+  write.table(tmp.filter, paste(sub(".txt",paste("_",threshold,".txt", sep=""),file.name), sep=""), sep="\t", col.names = NA)
+
+
   setwd(home)
 
 }
