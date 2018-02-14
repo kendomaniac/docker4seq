6a23d6bc73183e05c3fd4c17c3508b8407d67c4d
kendomaniac <raffaele.calogero@gmail.com>
2018-02-14 16:01:31 +0100

    fixing bug

--- R/topX.R
+++ R/topX.R
@@ -3,7 +3,7 @@
 #' @param data.folder, a character string indicating the folder where input data are located and where output will be written
 #' @param file.name, counts table name, tab delimited. Matrix data file must be in data.folder.
 #' @param threshold, integer used for filtering indicate the number of top expressed genes to be selected
-#' @param log, boolean TRUE or FALSE, if FALSE gene expression data are expressed in the plot as log10.
+#' @param logged, boolean TRUE or FALSE, if FALSE gene expression data are expressed in the plot as log10.
 #'
 #' @author Name Family name, myemail [at] somewhere [dot] org, Affiliation
 #'
@@ -12,7 +12,7 @@
 #' @examples
 #'\dontrun{
 #'
-#'  topx(data.folder="path/of/data/folder",file.name="matrixName",threshold=100000, log=FALSE)
+#'  topx(data.folder="path/of/data/folder",file.name="matrixName",threshold=100000, logged=FALSE)
 #' }
 #'
 #' @export
@@ -22,7 +22,7 @@ topx <- function(data.folder ,file.name, threshold, log=FALSE){
   tmp <- read.table(file.name, header=T, sep="\t", row.names=1)
   sum.counts <- apply(tmp, 1, sum)
   pdf("gene_expression_distribution.pdf")
-  if(!log10){
+  if(!logged){
     hist(log10(sum.counts), xlab="log10 gene counts summary", breaks=100)
   }else{
     hist(sum.counts, xlab="log gene counts summary", breaks=100)
