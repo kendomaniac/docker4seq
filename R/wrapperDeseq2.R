d535991957a23ff276f8592cf3fb6e20f9dbab5a
kendomaniac <raffaele.calogero@gmail.com>
2018-09-28 07:26:45 +0200

    update

--- R/wrapperDeseq2.R
+++ R/wrapperDeseq2.R
@@ -69,6 +69,7 @@ wrapperDeseq2 <- function(output.folder, group=c("sudo","docker"), experiment.ta
   }
 
   system("mv genes4david.txt genes2GO.txt")
+  system("mv gbkg4david.txtt gbkg2GO.txt")
 
   #running time 2
   ptm <- proc.time() - ptm
