65484a303df17ff549750c494a1c3ca2f402b611
kendomaniac <raffaele.calogero@gmail.com>
2018-11-01 19:06:53 +0100

    update

---
+++ R/downloadGeneInfo.R
@@ -0,0 +1,71 @@
+#' @title Downloading the full set of genes information from HGNC
+#' @description This function retrieves the full table of curated genes annotations
+#'
+#' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
+#' @param data.folder, a character string indicating where downloaded data will be stored
+#' @param url, the url to download the full dataset
+#' @author Raffaele Calogero
+#'
+#' @return one file files: gene_with_protein_product.txt
+#' @examples
+#'\dontrun{
+#'     getInfo(group="docker",data.folder=getwd(),
+#'     url="ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/locus_types/gene_with_protein_product.txt")
+#'
+#' }
+#' @export
+getInfo <- function(group=c("sudo","docker"), data.folder=getwd(), url){
+  home <- getwd()
+  setwd(data.folder)
+
+  #initialize status
+  system("echo 0 >& ExitStatusFile")
+
+  #running time 1
+  ptm <- proc.time()
+  #running time 1
+  test <- dockerTest()
+  if(!test){
+    cat("\nERROR: Docker seems not to be installed in your system\n")
+    system("echo 10 >& ExitStatusFile")
+    setwd(home)
+    return(10)
+  }
+  params <- paste("--cidfile ",data.folder,"/dockerID -v ",data.folder,":/data -d docker.io/repbioinfo/genes.2018.01 sh /bin/getinfo.sh ", url, sep="")
+  resultRun <- runDocker(group=group, params=params)
+
+  if(resultRun==0){
+    system(paste("cp ", docker_fastq.folder, "/* ", fastq.folder, sep=""))
+  }
+  #running time 2
+  ptm <- proc.time() - ptm
+  dir <- dir(data.folder)
+  dir <- dir[grep("run.info",dir)]
+  if(length(dir)>0){
+    con <- file("run.info", "r")
+    tmp.run <- readLines(con)
+    close(con)
+    tmp.run[length(tmp.run)+1] <- paste("user run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+    writeLines(tmp.run,"run.info")
+  }else{
+    tmp.run <- NULL
+    tmp.run[1] <- paste("run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+
+    writeLines(tmp.run,"run.info")
+  }
+
+  #saving log and removing docker container
+  container.id <- readLines(paste(fastq.folder,"/dockerID", sep=""), warn = FALSE)
+  system(paste("docker logs ", container.id, " >& ", "bwa_",substr(container.id,1,12),".log", sep=""))
+  system(paste("docker rm ", container.id, sep=""))
+
+  system(paste("cp ",paste(path.package(package="docker4seq"),"containers/containers.txt",sep="/")," ",fastq.folder, sep=""))
+  setwd(home)
+}
+
+
+
