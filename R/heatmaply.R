091c095b6d3cb02da006c67f45bf7bcaacb8ea5b
Nicola Licheri <nicola.licheri@gmail.com>
2019-05-07 12:22:22 +0200

    adding ranges heatmap signals

--- R/heatmaply.R
+++ R/heatmaply.R
@@ -1,41 +1,42 @@
-#' @title Function to produce an interactive heatmap using plotly 
+#' @title Function to produce an interactive heatmap using plotly
 #' @description This function generates an heatmap using a count table and a specific gene list
 #' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
 #' @param scratch.folder, a character string indicating the scratch folder where docker container will be mounted
-#' @param count.table, a character string indicating the path of the counts table file 
-#' @param gene.list, a character string indicating the path of the file containing the genes to include in the heatmap 
+#' @param count.table, a character string indicating the path of the counts table file
+#' @param gene.list, a character string indicating the path of the file containing the genes to include in the heatmap
 #' @param output.folder, a character string indicating the path of the output folder
 #' @param separator, a character string indicating the separator character in the count table. Allowed characters are TAB ('\t'), COMMA (',') and SPACE (' ')
 #' @param status, a character string, 'raw' if the data are raw counts, 'log' otherwise
 #' @author Nicola Licheri
 #'
-#' @return 
+#' @return
 #' @examples
 #'\dontrun{
 #'
 #' }
 #' @export
 
-heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, gene.list, output.folder, 
-                      separator=c("TAB", "COMMA", "SPACE"), status=c("raw", "log")) {
-  
+heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, gene.list, output.folder,
+                      separator=c("TAB", "COMMA", "SPACE"), status=c("raw", "log"),
+                      lower.range=NA, upper.range=NA) {
+
   #running time 1
   ptm <- proc.time()
-  
+
   #obtaining absolute paths
   count.table <- normalizePath(count.table)
   gene.list <- normalizePath(gene.list)
   scratch.folder <- normalizePath(scratch.folder)
   output.folder <- normalizePath(output.folder)
-  
+
   data.folder <- output.folder
-  
-  #storing the position of the home folder  
+
+  #storing the position of the home folder
   home <- getwd()
   setwd(data.folder)
   #initialize status
   system("echo 0 > ExitStatusFile 2>&1")
-  
+
   #check  if scratch folder exist
   if (!file.exists(scratch.folder)){
     cat(paste("\nIt seems that ",scratch.folder, " folder does not exist\n"))
@@ -43,21 +44,26 @@ heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, ge
     setwd(data.folder)
     return(3)
   }
-  
+
+  if (is.na(lower.range) || is.na(upper.range)) {
+      lower.range = 0
+      upper.range = 0
+  }
+
   params <- paste(
     "--cidfile", paste0(data.folder, "/dockerID"),
-    "-v", paste0(count.table, ":/data/in/count_table"), 
+    "-v", paste0(count.table, ":/data/in/count_table"),
     "-v", paste0(gene.list, ":/data/in/gene_list"),
-    "-v", paste0(output.folder, ":/data/out"),  
-    "-v", paste0(scratch.folder, ":/scratch"), 
-    "-d", "docker.io/cursecatcher/heatmap", "/bin/bash /bin/heatmap.sh", count.table, separator, status
+    "-v", paste0(output.folder, ":/data/out"),
+    "-v", paste0(scratch.folder, ":/scratch"),
+    "-d", "docker.io/cursecatcher/heatmap", "/bin/bash /bin/heatmap.sh", count.table, separator, status, lower.range, upper.range
   )
   resultRun <- runDocker(group=group, params=params)
-  
+
   if (resultRun == 0) {
     cat("Heatmaply terminated successfully.\n")
   }
-  
+
   #running time 2
   ptm <- proc.time() - ptm
   dir <- dir(data.folder)
@@ -75,10 +81,10 @@ heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, ge
     tmp.run[1] <- paste("run time mins ",ptm[1]/60, sep="")
     tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
     tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
-    
+
     writeLines(tmp.run,"run.info")
   }
-  
+
   #saving log and removing docker container
   container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
   system(paste("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/", "heatmaply_", substr(container.id,1,12),".log", sep=""))
