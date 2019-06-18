bf1ba16520ab606fb8f3eb4fd13807d6b5f36ad5
Nicola Licheri <nicola.licheri@gmail.com>
2019-06-18 18:09:21 +0200

    fix parameter

--- R/heatmaply.R
+++ R/heatmaply.R
@@ -7,6 +7,7 @@
 #' @param output.folder, a character string indicating the path of the output folder
 #' @param separator, a character string indicating the separator character in the count table. Allowed characters are TAB, COMMA  and SPACE
 #' @param status, a character string, 'raw' if the data are raw counts, 'log' otherwise
+#' @param color.palette, a string indicating the color palette to be used in the heatmap. Available palettes are Viridis, BrBG, magma, plasma and cividis
 #' @author Nicola Licheri, nicola [dot] licheri [at] unito [dot] it, University of Turin
 #'
 #' @return A html file containing the interactive heatmap produced using plot.ly
@@ -38,11 +39,34 @@ heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, ge
 
   #check  if scratch folder exist
   if (!file.exists(scratch.folder)){
-    cat(paste("\nIt seems that ",scratch.folder, " folder does not exist\n"))
+    cat(paste("\nIt seems that", scratch.folder, "folder does not exist\n"))
     system("echo 3 > ExitStatusFile 2>&1")
     setwd(data.folder)
     return(3)
   }
+  
+  #check if output folder exist 
+  if (!file.exists(output.folder)) {
+    cat(paste("\nIt seems that", output.folder, "folder does not exist\n"))
+    system("echo 3 > ExitStatusFile 2>&1")
+    setwd(data.folder)
+    return(3)
+  }
+  
+  #check if input data actually exist
+  if (!file.exists(count.table)) {
+    cat(paste("\nIt seems that", count.table, "file does not exist\n"))
+    system("echo 3 > ExitStatusFile 2>&1")
+    setwd(data.folder)
+    return(3)
+  }
+  if (!file.exists(gene.list)) {
+    cat(paste("\nIt seems that", gene.list, "file does not exist\n"))
+    system("echo 3 > ExitStatusFile 2>&1")
+    setwd(data.folder)
+    return(3)
+  }
+  
 
   params <- paste(
     "--cidfile", paste0(data.folder, "/dockerID"),
@@ -50,7 +74,8 @@ heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, ge
     "-v", paste0(gene.list, ":/data/in/gene_list"),
     "-v", paste0(output.folder, ":/data/out"),
     "-v", paste0(scratch.folder, ":/scratch"),
-    "-d", "docker.io/cursecatcher/heatmap", "/bin/bash /bin/heatmap.sh", color.palette, separator, status# count.table, separator, status
+    "-d", "docker.io/cursecatcher/heatmap", 
+    "/bin/bash /bin/heatmap.sh", color.palette, separator, status
   )
   resultRun <- runDocker(group=group, params=params)
 
@@ -83,8 +108,8 @@ heatmaply <- function(group=c("docker", "sudo"), scratch.folder, count.table, ge
 
   #saving log and removing docker container
   container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
-  system(paste("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/", "heatmaply_", substr(container.id,1,12),".log", sep=""))
-  system(paste("docker rm ", container.id, sep=""))
+  system(paste0("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/", "heatmaply_", substr(container.id,1,12),".log"))
+  system(paste("docker rm", container.id))
   # removing temporary files
   cat("\n\nRemoving the temporary file ....\n")
   system("rm -fR out.info")
