30de3777149b2dea623c8a6ea550bcb069b542b7
giuferrero <giulio.ferrero@unito.it>
2022-04-07 15:58:33 +0200

    Update bwaIndex.R
    Update RNA central versione

--- R/bwaIndex.R
+++ R/bwaIndex.R
@@ -117,7 +117,7 @@ bwaIndex <- function(group=c("sudo","docker"), genome.folder=getwd(), genome.url
 ### BWA index case for non miRNA ncRNAs
 	if(mode=="ncRNA"){
 
-	  rc_ok_ver = paste0(seq(1, 16, by=1), ".0")
+	  rc_ok_ver = paste0(seq(1, 20, by=1), ".0")
 
 	  rc.version = paste0(rc.version, ".0")
 
