6ae978dda6d6024c294b765c1b9eb540ed45d8be
giuferrero <giulio.ferrero@unito.it>
2024-04-04 23:26:25 +0200

    Update bwaIndex.R
    change the miRNA part to require a direct link to hairpin and mature miRNA sequences

--- R/bwaIndex.R
+++ R/bwaIndex.R
@@ -8,7 +8,8 @@
 #' @param gtf.url, a character string indicating the URL from ENSEMBL ftp for the GTF for genome of interest
 #' @param dbsnp.file, a character string indicating the name of dbSNP vcf located in the genome folder. The dbSNP vcf, dbsnp_138.b37.vcf.gz and dbsnp_138.hg19.vcf.idx.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37
 #' @param g1000.file, a character string indicating the name of 1000 genome vcf located in the genome folder. The 1000 genomes vcf, Mills_and_1000G_gold_standard.indels.b37.vcf.gz and Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.idx.gz, can be downloaded from ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/
-#' @param mb.version, a character string indicating the required version of miRBase database. Visit http://www.mirbase.org to select the proper version number.
+#' @param mb.url.haripin, character string indicating the link to the hairpin miRNA sequences miRBase database. Visit http://www.mirbase.org to select the proper version number.
+#' @param mb.url.mature, a character string indicating the link to the mature miRNA sequences from miRBase database. Visit http://www.mirbase.org to select the proper version number.
 #' @param mb.species, a character string indicating the name of a species annotated in miRBase (e.g. "hsa" for human miRNAs). Please refer to http://www.mirbase.org/help/genome_summary.shtml to proper species name.
 #' @param rc.version, a character string indicating the required version of RNA Central database. Visit ftp://ftp.ebi.ac.uk/pub/databases/RNAcentral/releases/ to select the proper version number.
 #' @param rc.species, a character string indicating the name of a species annotated in RNA Central (e.g. "Homo sapiens" for human ncRNAs). Please refer to NCBI taxonomy annotations at https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi to proper species name.
@@ -26,7 +27,7 @@
 #'     bwaIndex(group="docker", genome.folder="/data/genomes", genome.url="http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz", dbsnp.file="dbsnp_138.hg19.vcf.gz", g1000.file="Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.gz", mode="GATK")
 #'
 #'     #running bwa index for miRNA analysis
-#'     bwaIndex(group="docker", genome.folder="/data/genomes", mb.version="CURRENT", mb.species="hsa", mode="miRNA")
+#'     bwaIndex(group="docker", genome.folder="/data/genomes", mb.url.mature=https://www.mirbase.org/download/CURRENT/mature.fa, mb.url.hairpin=https://www.mirbase.org/download/CURRENT/hairpin.fa, mb.species="hsa", mode="miRNA")
 #'
 #'     #running bwa index for ncRNA analysis
 #'     bwaIndex(group="docker", genome.folder="/data/genomes/hg19_bwa", rc.version="9", rc.species="Homo sapiens", length=80, mode="ncRNA")
@@ -89,20 +90,12 @@ bwaIndex <- function(group=c("sudo","docker"), genome.folder=getwd(), genome.url
       system(paste("gzip -d ", sub(".vcf.gz$", ".vcf.idx.gz$",g1000.file), sep=""))
       system(paste("mv ", sub(".gz$", "",g1000.file), " g1k.vcf", sep=""))
     }
- params <- paste("--cidfile ", genome.folder, "/dockerID -v ",     genome.folder,":/data/scratch", " -d docker.io/repbioinfo/bwaindex sh  /bin/bwa.index.sh ", genome.folder, " ", mode, " ", genome.url,   sep="")
+ params <- paste("--cidfile ", genome.folder, "/dockerID -v ", genome.folder,":/data/scratch", " -d docker.io/repbioinfo/bwaindex sh  /bin/bwa.index.sh ", genome.folder, " ", mode, " ", genome.url,   sep="")
   }
 
 ### BWA index case for miRNA
 	if(mode=="miRNA"){
 
-	  mb_ok_ver = c("1.0","10.0","10.1","1.1","11.0","1.2","12.0","1.3","13.0","14","1.4","15","1.5","16","17","18","19","20","2.0","21","2.1","22","2.2","3.0","3.1","4.0","5.0","5.1","6.0","7.0","7.1","8.0","8.1","8.2","9.0","9.1","9.2", "CURRENT")
-
-    if(mb.version %in% mb_ok_ver == FALSE){
-      cat("\nThe miRBase version is not correct\n")
-      system("echo 2 > ExitStatusFile 2>&1")
-      setwd(home)
-      return(2)
-    }
 	  if(is.null(mb.species)){
 	    cat("\nPlease insert a proper miRBase species identifier\n")
 	    system("echo 2 > ExitStatusFile 2>&1")
@@ -110,7 +103,7 @@ bwaIndex <- function(group=c("sudo","docker"), genome.folder=getwd(), genome.url
 	    return(2)
 	  }
 	  else{
-	    params <- paste("--cidfile ", genome.folder, "/dockerID -v ", genome.folder,":/data/scratch", " -d docker.io/repbioinfo/bwaindex bash /bin/bwa.index.sh ", genome.folder, " ", mode, " ", mb.version, " ", mb.species, sep="")
+	    params <- paste("--cidfile ", genome.folder, "/dockerID -v ", genome.folder,":/data/scratch", " -d docker.io/repbioinfo/bwaindex bash /bin/bwa.index.sh ", genome.folder, " ", mode, " ", mb.url.hairpin, " ", mb.url.mature, " ", mb.species, sep="")
 	  }
 	}
 
